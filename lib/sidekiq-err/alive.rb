34749ecf18732debe0fd8178cca7076ad1a6292a
Campbell Allen <campbell.allen@gmail.com>
2021-12-09 18:05:06 +0000

    add hostname checking for alive

--- lib/sidekiq-err/alive.rb
+++ lib/sidekiq-err/alive.rb
@@ -2,9 +2,21 @@
 
 module SidekiqErr
   class Alive
-    def self.check?
+    def self.check?(hostname)
       process_set = Sidekiq::ProcessSet.new
-      raise(NoProcessFound, 'No sidekiq process running!') if process_set.size.zero?
+      # now search for the provided process name in the process set
+      found_process = process_set.find do |process|
+        # we compare the process hostname here as we can know this from k8s land pod
+        # and can use the $HOSTNAME env var in the probe command in the probe specification
+        #
+        # https://stackoverflow.com/questions/58800495/get-a-kubernetes-pods-full-id-from-inside-of-itself-running-container
+        # Note: this aligns to 1 sidekiq container process per pod
+        #         this may not hold true in some deployments but should work for most use cases
+        #         it's preferable to use pod auto scaling vs running multiple containers in a pod
+        process['hostname'] == hostname
+      end
+
+      raise(NoProcessFound, "No sidekiq process found for hostname: #{hostname}") unless found_process
     end
   end
 end
--- spec/alive_spec.rb
+++ spec/alive_spec.rb
@@ -3,20 +3,46 @@
 require 'spec_helper'
 
 RSpec.describe SidekiqErr::Alive do
-  let(:process_set) do
-    instance_double('Sidekiq::ProcessSet', size: 1)
+  let(:hostname) { 'a03dc1421fe6' }
+  let(:process_set) { instance_double('Sidekiq::ProcessSet', size: 1) }
+  let(:process) do
+    Sidekiq::Process.new(
+      'hostname' => hostname,
+      'started_at' => 1594054991.2349708,
+      'pid' => 55,
+      'tag' => '',
+      'concurrency' => 10,
+      'queues' => ['default'],
+      'labels' => [],
+      'identity' => 'a03dc1421fe6:55:b94b1ede3adf',
+      'busy' => 0,
+      'beat' => 1594055026.3869414,
+      'quiet' => 'false'
+    )
+  end
+
+  before do
+    allow(process_set).to receive(:find).and_yield(process)
   end
 
   describe '.is_running?' do
     it 'raises an error if no sidekiq processes is present' do
       expect do
-        described_class.check?
-      end.to raise_error(SidekiqErr::NoProcessFound, 'No sidekiq process running!')
+        described_class.check?(hostname)
+      end.to raise_error(SidekiqErr::NoProcessFound, "No sidekiq process found for hostname: #{hostname}")
     end
 
-    it 'does not raise if sidekiq processes is present' do
+    it 'does not raise if sidekiq processes name can be found' do
       allow(Sidekiq::ProcessSet).to receive(:new).and_return(process_set)
-      expect { described_class.check? }.not_to raise_error
+      expect { described_class.check?(hostname) }.not_to raise_error
+    end
+
+    it 'raises if the process name can not be found' do
+      allow(Sidekiq::ProcessSet).to receive(:new).and_return(process_set)
+
+      expect do
+        described_class.check?('wat-host')
+      end.to raise_error(SidekiqErr::NoProcessFound, "No sidekiq process found for hostname: wat-host")
     end
   end
 end
